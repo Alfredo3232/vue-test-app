a71b3ea62f36f90d5bab14d3d19ae87626442819
ARojas32 <Alfredo2001@me.com>
2022-10-14 18:03:31 -0500

    add functionality to FriendContact to show friends including CSS

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "jenny",
+        name: "Jenny",
+        phone: "012 3456 7890",
+        email: "jenny@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
