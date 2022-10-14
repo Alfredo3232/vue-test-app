284ba1a996568b0165d693f7c851e3e4b3dc343c
ARojas32 <Alfredo2001@me.com>
2022-10-14 16:20:44 -0500

    lint changes to html, & added our own vue code to display possible friends

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "jenny",
+          name: "Jenny",
+          phone: "012 3456 7890",
+          email: "jenny@localhost.com",
+        },
+        {
+          id: "john",
+          name: "John Wick",
+          phone: "098 7654 3210",
+          email: "wick@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
